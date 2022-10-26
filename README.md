e4d8ff1169067fe5b8896ad48f7c012ddcc186b8
Martin Vysny <mavi@vaadin.com>
2022-10-26 09:20:38 +0300

    Add Gradle

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# vaadin-boot
-Boots your Vaadin app in embedded Jetty from your main() method quickly and easily
+# Vaadin Boot
+
+Boots your Vaadin app in embedded Jetty from your `main()` method quickly and easily.
+
+Tired of complexity of running your app as WAR from your IDE? Tired of using Spring Boot just to start your app in a simple way?
+Tired of constant debugging issues? Then this project is for you.
+
+TODO work in progress
---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+
+plugins {
+    id("java")
+}
+
+defaultTasks("clean", "build")
+
+// don't use Jetty 11+ - uses jakarta.Servlet instead of javax.Servlet which makes it incompatible with Vaadin
+// Jetty 10+ requires Java 11+
+val jettyVersion = "10.0.11"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.slf4j:slf4j-api:2.0.0")
+    implementation("org.jetbrains:annotations:22.0.0")
+
+    // Embedded Jetty dependencies
+    implementation("org.eclipse.jetty:jetty-webapp:${jettyVersion}")
+    implementation("org.eclipse.jetty.websocket:websocket-javax-server:${jettyVersion}")
+
+    testImplementation("org.slf4j:slf4j-simple:2.0.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.1")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        // to see the exceptions of failed tests in CI
+        exceptionFormat = TestExceptionFormat.FULL
+    }
+}
