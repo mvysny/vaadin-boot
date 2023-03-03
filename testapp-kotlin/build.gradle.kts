d90dc315084e6e893340cf007f96d720faf1a328
Martin Vysny <mavi@vaadin.com>
2023-03-03 12:17:37 +0200

    Example apps: exclude dev server in production builds

--- testapp-kotlin/build.gradle.kts
+++ testapp-kotlin/build.gradle.kts
@@ -7,7 +7,13 @@ plugins {
 dependencies {
     implementation(project(":vaadin-boot"))
     implementation("org.slf4j:slf4j-simple:${properties["slf4j_version"]}")
-    implementation("com.vaadin:vaadin-core:${properties["vaadin_version"]}")
+    implementation("com.vaadin:vaadin-core:${properties["vaadin_version"]}") {
+        afterEvaluate {
+            if (vaadin.productionMode) {
+                exclude(module = "vaadin-dev-server")
+            }
+        }
+    }
 
     implementation("io.javalin:javalin:5.3.2") {
         exclude(group = "org.eclipse.jetty")
