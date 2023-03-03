335a3919e5d4daf2dd7191bcf897787ea3222a8b
Martin Vysny <mavi@vaadin.com>
2023-03-03 12:17:37 +0200

    Example apps: exclude dev server in production builds

--- testapp/build.gradle.kts
+++ testapp/build.gradle.kts
@@ -6,7 +6,13 @@ plugins {
 dependencies {
     implementation(project(":vaadin-boot"))
     implementation("org.slf4j:slf4j-simple:${properties["slf4j_version"]}")
-    implementation("com.vaadin:vaadin-core:${properties["vaadin_version"]}")
+    implementation("com.vaadin:vaadin-core:${properties["vaadin_version"]}") {
+        afterEvaluate {
+            if (vaadin.productionMode) {
+                exclude(module = "vaadin-dev-server")
+            }
+        }
+    }
 
     testImplementation("com.github.mvysny.kaributesting:karibu-testing-v23:${properties["kaributesting_version"]}")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:${properties["junit_version"]}")
