02fb1cae793072d79a31c87b4794052a72abded9
Martin Vysny <mavi@vaadin.com>
2023-03-07 22:50:18 +0200

    11.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "com.github.mvysny.vaadin-boot"
-    version = "11.0-SNAPSHOT"
+    version = "11.0"
 
     repositories {
         mavenCentral()
