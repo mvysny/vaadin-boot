0d8ea0c734474eabe2d43b3f51bcd526a66dc9cc
Martin Vysny <mavi@vaadin.com>
2023-03-17 07:36:27 +0200

    Env: more verbose logging of /webapp Jetty resource

--- vaadin-boot/src/main/java/com/github/mvysny/vaadinboot/VaadinBoot.java
+++ vaadin-boot/src/main/java/com/github/mvysny/vaadinboot/VaadinBoot.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.Servlet;
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.MalformedURLException;
 import java.time.Duration;
