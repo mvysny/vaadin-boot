a7d9dcf655db7b2b62b8242c20e7db6ff4317bdb
Martin Vysny <mavi@vaadin.com>
2023-08-08 15:39:20 +0300

    minor

--- testapp-kotlin/src/test/kotlin/com/example/MyJavalinServletTest.kt
+++ testapp-kotlin/src/test/kotlin/com/example/MyJavalinServletTest.kt
@@ -15,7 +15,7 @@ class MyJavalinServletTest {
     fun startJetty() {
         val ctx = WebAppContext()
         // This used to be EmptyResource, but it got removed in Jetty 12. Let's use some dummy resource instead.
-        ctx.baseResource = ctx.resourceFactory.newClassPathResource("com/example/MyJavalinServletTest.class")
+        ctx.baseResource = ctx.resourceFactory.newClassPathResource("java/lang/String.class")
         ctx.addServlet(MyJavalinServlet::class.java, "/rest/*")
         server = Server(30123)
         server!!.handler = ctx
