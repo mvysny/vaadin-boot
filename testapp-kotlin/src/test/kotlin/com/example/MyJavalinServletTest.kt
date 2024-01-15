f763262e3c302ae904aeef33fe2cb342e0ceda8d
Martin Vysny <mavi@vaadin.com>
2024-01-15 16:28:20 +0200

    dep v bump

--- testapp-kotlin/src/test/kotlin/com/example/MyJavalinServletTest.kt
+++ testapp-kotlin/src/test/kotlin/com/example/MyJavalinServletTest.kt
@@ -5,6 +5,7 @@ import org.eclipse.jetty.server.Server
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.net.URI
 import java.net.URL
 import kotlin.test.assertEquals
 
@@ -15,7 +16,7 @@ class MyJavalinServletTest {
     fun startJetty() {
         val ctx = WebAppContext()
         // This used to be EmptyResource, but it got removed in Jetty 12. Let's use some dummy resource instead.
-        ctx.baseResource = ctx.resourceFactory.newClassPathResource("java/lang/String.class")
+        ctx.baseResource = ctx.resourceFactory.newClassLoaderResource("java/lang/String.class")
         ctx.addServlet(MyJavalinServlet::class.java, "/rest/*")
         server = Server(30123)
         server!!.handler = ctx
@@ -29,6 +30,6 @@ class MyJavalinServletTest {
 
     @Test
     fun testRest() {
-        assertEquals("Hello!", URL("http://localhost:30123/rest").readText())
+        assertEquals("Hello!", URI("http://localhost:30123/rest").toURL().readText())
     }
 }
