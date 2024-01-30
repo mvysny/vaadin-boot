2d66c50cd3db20e6b2947c0b66ec3e3bbf4b31b8
Martin Vysny <mavi@vaadin.com>
2024-01-30 16:48:47 +0200

    Gradle: take version catalog into use

--- vaadin-boot/build.gradle.kts
+++ vaadin-boot/build.gradle.kts
@@ -1,28 +1,23 @@
-val vaadin_version: String by extra
-val slf4j_version: String by extra
-val junit_version: String by extra
-val jetty_version: String by extra
-
 plugins {
     `java-library`
 }
 
 dependencies {
-    implementation("org.slf4j:slf4j-api:$slf4j_version")
-    api("org.jetbrains:annotations:24.0.1")
+    implementation(libs.slf4j.api)
+    api(libs.jetbrains.annotations)
 
     // Embedded Jetty dependencies.
     // This one is needed to host webapps and perform classpath scanning for annotations
-    api("org.eclipse.jetty.ee10:jetty-ee10-annotations:$jetty_version")
+    api(libs.jetty.webapp)
     // This one is required to have websocket/push support.
-    implementation("org.eclipse.jetty.ee10.websocket:jetty-ee10-websocket-jakarta-server:$jetty_version")
+    implementation(libs.jetty.websocket)
 
-    // opens url in a browser; Vaadin dependency
-    implementation("com.vaadin:open:8.5.0")
+    // opens url in a browser
+    implementation(libs.vaadin.open)
 
-    testImplementation("org.slf4j:slf4j-simple:$slf4j_version")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:$junit_version")
-    testImplementation("com.vaadin:vaadin-core:$vaadin_version")
+    testImplementation(libs.slf4j.simple)
+    testImplementation(libs.junit.jupiter.engine)
+    testImplementation(libs.vaadin.core)
 }
 
 val configureMavenCentral = ext["configureMavenCentral"] as (artifactId: String) -> Unit
