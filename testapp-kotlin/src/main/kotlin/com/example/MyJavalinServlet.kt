beb541e17b06b15c9efaa439250d04c65447fb0b
Martin Vysny <mavi@vaadin.com>
2024-02-28 13:20:55 +0200

    #23 Initial port to Javalin6

--- testapp-kotlin/src/main/kotlin/com/example/MyJavalinServlet.kt
+++ testapp-kotlin/src/main/kotlin/com/example/MyJavalinServlet.kt
@@ -1,8 +1,9 @@
 package com.example
 
 import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder.get
 import io.javalin.http.Context
-import io.javalin.http.servlet.JavalinServlet
+import jakarta.servlet.Servlet
 import jakarta.servlet.annotation.WebServlet
 import jakarta.servlet.http.HttpServlet
 import jakarta.servlet.http.HttpServletRequest
@@ -10,9 +11,11 @@ import jakarta.servlet.http.HttpServletResponse
 
 @WebServlet(name = "MyJavalinServlet", urlPatterns = ["/rest/*"])
 class MyJavalinServlet : HttpServlet() {
-    private val javalin: JavalinServlet = Javalin.createStandalone()
-        .get("/rest") { ctx: Context -> ctx.result("Hello!") }
-        .javalinServlet()
+    private val javalin: Servlet = Javalin.create {
+        it.router.apiBuilder {
+            get("/rest") { ctx: Context -> ctx.result("Hello!") }
+        }
+    } .javalinServlet()
 
     override fun service(req: HttpServletRequest, resp: HttpServletResponse) {
         javalin.service(req, resp)
