c9844c3a5d1c0756125d77ad1aa1756cac57baac
Martin Vysny <mavi@vaadin.com>
2024-09-02 09:20:23 +0300

    #27 JettyTest refactoring

---
+++ testapp/src/test/java/com/example/TestUtils.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+public class TestUtils {
+    @NotNull
+    public static String wget(@NotNull String url) throws IOException, InterruptedException {
+        try (HttpClient client = HttpClient.newBuilder().build()) {
+            final HttpRequest request = HttpRequest.newBuilder(URI.create(url))
+                    .timeout(Duration.ofSeconds(3))
+                    .build();
+            final HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() != 200) {
+                throw new IOException(response + " failed: " + response.body());
+            }
+            final String body = response.body();
+            return body;
+        }
+    }
+}
