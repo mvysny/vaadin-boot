9ac09107ca9599e3195006c4a81160267b03c270
Martin Vysny <mavi@vaadin.com>
2024-09-02 09:25:54 +0300

    #27 fix compat with jdk 17

--- testapp-tomcat/src/test/java/com/example/TestUtils.java
+++ testapp-tomcat/src/test/java/com/example/TestUtils.java
@@ -12,16 +12,15 @@ import java.time.Duration;
 public class TestUtils {
     @NotNull
     public static String wget(@NotNull String url) throws IOException, InterruptedException {
-        try (HttpClient client = HttpClient.newBuilder().build()) {
-            final HttpRequest request = HttpRequest.newBuilder(URI.create(url))
-                    .timeout(Duration.ofSeconds(3))
-                    .build();
-            final HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            if (response.statusCode() != 200) {
-                throw new IOException(response + " failed: " + response.body());
-            }
-            final String body = response.body();
-            return body;
+        final HttpClient client = HttpClient.newBuilder().build();
+        final HttpRequest request = HttpRequest.newBuilder(URI.create(url))
+                .timeout(Duration.ofSeconds(3))
+                .build();
+        final HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() != 200) {
+            throw new IOException(response + " failed: " + response.body());
         }
+        final String body = response.body();
+        return body;
     }
 }
