f696f1dcfbd55a00ac82addfe8b953afa29ce242
Martin Vysny <mavi@vaadin.com>
2024-09-02 10:33:30 +0300

    #27 minor

--- vaadin-boot/src/test/java/com/github/mvysny/vaadinboot/VaadinBootTest.java
+++ vaadin-boot/src/test/java/com/github/mvysny/vaadinboot/VaadinBootTest.java
@@ -1,14 +1,6 @@
 package com.github.mvysny.vaadinboot;
 
-import org.eclipse.jetty.ee10.webapp.WebAppContext;
-import org.jetbrains.annotations.NotNull;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class VaadinBootTest {
     @Test
