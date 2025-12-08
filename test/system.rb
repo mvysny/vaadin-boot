7c9966edb58226fb476d388e3356c22b5ce6faaf
Martin Vysny <mavi@vaadin.com>
2025-12-08 10:24:09 +0200

    system test: upgrade

--- test/system.rb
+++ test/system.rb
@@ -13,8 +13,18 @@ end
 
 require 'timeout'
 
+# Checks if process is running.
+def running?(pid)
+  Process.kill(0, pid)
+  true
+rescue Errno::ERSCH
+  false
+end
+
 # Awaits for process to terminate cleanly. If it doesn't, the process is killed and an exception is raised.
 def wait_for(pid, seconds = 5)
+  return unless running? pid
+
   Timeout.timeout(seconds) do
     Process.wait(pid)
     puts "#{pid}: exited cleanly"
@@ -43,32 +53,55 @@ def wget(url)
   uri = URI(url)
   response = Net::HTTP.get_response(uri)
   raise "#{url} failed: #{response.body}" unless response.code == '200'
+
   body = response.body
   raise 'Not a Vaadin index.ts' unless body.include? 'window.Vaadin'
+
   puts "#{url}: OK"
+end
+
+# Reads given IO fully and returns {String}. Handles {Errno::EIO} gracefully.
+# @return [String] stdin+stderr
+def read_fully(reader)
+  output = []
+  begin
+    while line = reader.gets
+      output << line
+    end
+  rescue Errno::EIO
+    # EIO = normal when child exits
+  end
+  output.join
 end
 
 require 'pty'
 require 'io/console'
 
 FileUtils.cd '..'
-exec './gradlew clean --no-daemon --info'
-exec './gradlew testapp:build -Pvaadin.productionMode -x test --no-daemon --info'
+# exec './gradlew clean --no-daemon --info'
+# exec './gradlew testapp:build -Pvaadin.productionMode -x test --no-daemon --info'
 FileUtils.cd 'testapp/build/distributions' do
-  exec 'tar xvf *.tar'
+  #  exec 'tar xvf *.tar'
   dir = Dir.glob('testapp-*').find { File.directory? it }
   FileUtils.cd "#{dir}/bin" do
     puts './testapp'
-    PTY.spawn('./testapp') do |read, write, pid|
+    PTY.spawn('./testapp') do |reader, write, pid|
       # Wait for the app to boot up
       sleep 4
       # Test that the app is up
+      raise 'Not running!' unless running? pid
+
       wget('http://localhost:8080')
 
       # All's good. Now test that the app dies when Enter is pressed.
-      puts "Sending Enter"
+      puts 'Sending Enter'
       write.puts # sends Enter, VaadinBoot should quit gracefully
       wait_for(pid, 5)
+    rescue StandardError => e
+      puts read_fully(reader)
+      raise e
+    ensure
+      write.close
     end
   end
 end
