2e505303c32b0318dd254debba7c6f30aab3e11e
Martin Vysny <mavi@vaadin.com>
2025-12-17 08:51:53 +0200

    tests

--- common/src/test/java/com/github/mvysny/vaadinboot/common/EnvTest.java
+++ common/src/test/java/com/github/mvysny/vaadinboot/common/EnvTest.java
@@ -6,26 +6,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class EnvTest {
 
-    @Test
-    public void flowBuildInfoJsonParsing() {
-        assertTrue(Env.flowBuildInfoJsonContainsProductionModeTrue("{\"productionMode\": true}"));
-        assertTrue(Env.flowBuildInfoJsonContainsProductionModeTrue("{\"productionMode\":true}"));
-        assertTrue(Env.flowBuildInfoJsonContainsProductionModeTrue("{\"productionMode\":\ntrue}"));
-        assertTrue(Env.flowBuildInfoJsonContainsProductionModeTrue("{\"productionMode\": true,\"foo\":\"bar\"}"));
-        assertTrue(Env.flowBuildInfoJsonContainsProductionModeTrue("{\"productionMode\":true,\"foo\":\"bar\"}"));
-        assertTrue(Env.flowBuildInfoJsonContainsProductionModeTrue("{\"productionMode\":\ntrue,\"foo\":\"bar\"}"));
-        assertFalse(Env.flowBuildInfoJsonContainsProductionModeTrue("{\"productionMode\": false}"));
-        assertFalse(Env.flowBuildInfoJsonContainsProductionModeTrue("{\"productionMode\":false}"));
-        assertFalse(Env.flowBuildInfoJsonContainsProductionModeTrue("{\"productionMode\":\nfalse}"));
-        assertFalse(Env.flowBuildInfoJsonContainsProductionModeTrue("{\"productionMode\": false, \"foo\":true}"));
-        assertFalse(Env.flowBuildInfoJsonContainsProductionModeTrue("{\"productionMode\":false, \"foo\":true}"));
-        assertFalse(Env.flowBuildInfoJsonContainsProductionModeTrue("{\"productionMode\":\nfalse, \"foo\":true}"));
-        assertFalse(Env.flowBuildInfoJsonContainsProductionModeTrue("{}"));
-        assertFalse(Env.flowBuildInfoJsonContainsProductionModeTrue("invalid json"));
-    }
+  @Test
+  public void flowBuildInfoJsonParsing() {
+    assertTrue(Env.flowBuildInfoJsonContainsProductionModeTrue("{\"productionMode\": true}"));
+    assertTrue(Env.flowBuildInfoJsonContainsProductionModeTrue("{\"productionMode\":true}"));
+    assertTrue(Env.flowBuildInfoJsonContainsProductionModeTrue("{\"productionMode\":\ntrue}"));
+    assertTrue(Env.flowBuildInfoJsonContainsProductionModeTrue("{\"productionMode\": true,\"foo\":\"bar\"}"));
+    assertTrue(Env.flowBuildInfoJsonContainsProductionModeTrue("{\"productionMode\":true,\"foo\":\"bar\"}"));
+    assertTrue(Env.flowBuildInfoJsonContainsProductionModeTrue("{\"productionMode\":\ntrue,\"foo\":\"bar\"}"));
+    assertFalse(Env.flowBuildInfoJsonContainsProductionModeTrue("{\"productionMode\": false}"));
+    assertFalse(Env.flowBuildInfoJsonContainsProductionModeTrue("{\"productionMode\":false}"));
+    assertFalse(Env.flowBuildInfoJsonContainsProductionModeTrue("{\"productionMode\":\nfalse}"));
+    assertFalse(Env.flowBuildInfoJsonContainsProductionModeTrue("{\"productionMode\": false, \"foo\":true}"));
+    assertFalse(Env.flowBuildInfoJsonContainsProductionModeTrue("{\"productionMode\":false, \"foo\":true}"));
+    assertFalse(Env.flowBuildInfoJsonContainsProductionModeTrue("{\"productionMode\":\nfalse, \"foo\":true}"));
+    assertFalse(Env.flowBuildInfoJsonContainsProductionModeTrue("{}"));
+    assertFalse(Env.flowBuildInfoJsonContainsProductionModeTrue("invalid json"));
+    assertTrue(Env.flowBuildInfoJsonContainsProductionModeTrue(
+        "{ \"productionMode\" : true, \n\"eagerServerLoad\" : false, \n\"react.enable\" : true, \n\"applicationIdentifier\" : \"app-744cac70c6fae7de4d924e4d434cc3a64c190ddc3ad2fdaa7f7fd2465ea8a08d\" \n}"));
+  }
 
-    @Test
-    public void smokeFindWebRoot() throws Exception {
-        assertNotNull(Env.findWebRoot());
-    }
+  @Test
+  public void smokeFindWebRoot() throws Exception {
+    assertNotNull(Env.findWebRoot());
+  }
 }
